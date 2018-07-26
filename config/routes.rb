36a25f9c3f84ec4f136b0e4cd0196d0427c647b9
Gabriel Alarcon <galarconesp@gmail.com>
2018-07-26 17:40:54 -0400

    commit 04: edit, update and destroy completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'todos/new'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
 end
