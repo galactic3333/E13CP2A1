e8e20de110a0775fe3b99eee706609aa049c8276
Gabriel Alarcon <galarconesp@gmail.com>
2018-07-26 17:07:29 -0400

    commit 03: create and show completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,20 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+
 end
